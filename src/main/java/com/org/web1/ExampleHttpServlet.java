2b6222bf0c335f00136d6f6edd89b5239779731c
Zylina <guptahimanshi002@gmail.com>
2022-02-24 19:50:22 +0530

    add

---
+++ src/main/java/com/org/web1/ExampleHttpServlet.java
@@ -0,0 +1,37 @@
+package com.org.web1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ExampleHttpServlet
+ */
+@WebServlet("/ExampleHttpServlet")
+public class ExampleHttpServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+	private String msg ;
+	public void init() throws ServletException {
+		msg = " Htpp Servlet Example " ;
+	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		pw.println("<h1>" +msg+"</h1>");
+		pw.println("<hr>");
+		pw.println("<p>"+ " Hello Friends !!!!"+"</p>");
+		}
+
+}
